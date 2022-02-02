ac437c2daaa2fb829853a70d0a5f6bf68fd8dc81
pepehonourable <91469944+pepehonourable@users.noreply.github.com>
2022-02-02 13:35:37 +0100

    Add files via upload
    first creation on the header to be used on all paged via "include"

---
+++ code/header.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+    <link rel="stylesheet" href="stylesheet.css"/>
+</head>
+<div class="header">
+    <div class="headerGrid">
+        <div class="headerTitle">
+            <li>NHL WEBSHOP</li>
+        </div>
+        <div class="search">
+        </div>
+        <nav>
+            <li>Home</li>
+            <li>Login</li>
+            <li>Admin</li>
+            <li>Cart</li>
+        </nav>
+    </div>
+</div>
+</html>
