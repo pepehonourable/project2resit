0dd875fee0b12086cc466bce51e9050f1c1a448b
pepehonourable <91469944+pepehonourable@users.noreply.github.com>
2022-02-04 11:54:06 +0100

    Add files via upload
    Added database made by Polly to Github. Made alterations to Order and Product Table. Added Quantity table (will change to orderline) and created relationships

---
+++ code/shop.sql
@@ -0,0 +1,168 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 04, 2022 at 11:52 AM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `shop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `OrderId` int(11) NOT NULL,
+  `TrackCode` int(11) NOT NULL,
+  `UserId` int(11) NOT NULL,
+  `ProductId` int(11) NOT NULL,
+  `Delivered` varchar(3) DEFAULT 'No',
+  `DateOrdered` date DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`OrderId`, `TrackCode`, `UserId`, `ProductId`, `Delivered`, `DateOrdered`) VALUES
+(2, 5, 1, 4, 'Yes', '2022-02-04'),
+(12, 9, 1, 5, 'No', '2022-02-04');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `ProductId` int(11) NOT NULL,
+  `Title` varchar(50) NOT NULL,
+  `Description` varchar(250) NOT NULL,
+  `Category` varchar(250) NOT NULL,
+  `Price` varchar(50) NOT NULL,
+  `Image` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`ProductId`, `Title`, `Description`, `Category`, `Price`, `Image`) VALUES
+(3, 'Tyler the Creator - Ziplock', 'What\'s the point of being rich when you wake up alone? / What\'s the point of going home when it ain\'t nobody there', 'Alt', '€9.99', 'tylerZiploc.jpg'),
+(4, 'Undertale soundtrack', 'Is there really any other game that can make you feel this way with just it’s music?', 'Soundtrack', '€19.99', 'undertaleSoundtrack.jpg'),
+(5, 'Royal blood - Space', 'Anything is possible when you put your mind to it. The result is music for your soul. You hear it in the lyrics and feel it with the beats pounding through your speakers. Royal Blood didn\'t disappoint! He gave us 100% on the CD.', 'Rock', '€29.99', 'royalBloodSpace.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quantity`
+--
+
+CREATE TABLE `quantity` (
+  `OrderId` int(11) NOT NULL,
+  `ProductId` int(11) NOT NULL,
+  `Quantity` int(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `UserName` varchar(20) NOT NULL,
+  `Password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `UserName`, `Password`) VALUES
+(1, 'Pepe', 'pepe');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`OrderId`,`TrackCode`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`ProductId`);
+
+--
+-- Indexes for table `quantity`
+--
+ALTER TABLE `quantity`
+  ADD KEY `ProductId` (`ProductId`),
+  ADD KEY `OrderId` (`OrderId`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `OrderId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `ProductId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `quantity`
+--
+ALTER TABLE `quantity`
+  ADD CONSTRAINT `quantity_ibfk_1` FOREIGN KEY (`OrderId`) REFERENCES `orders` (`orderId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `quantity_ibfk_2` FOREIGN KEY (`ProductId`) REFERENCES `product` (`ProductId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `quantity_ibfk_3` FOREIGN KEY (`OrderId`) REFERENCES `orders` (`orderId`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
