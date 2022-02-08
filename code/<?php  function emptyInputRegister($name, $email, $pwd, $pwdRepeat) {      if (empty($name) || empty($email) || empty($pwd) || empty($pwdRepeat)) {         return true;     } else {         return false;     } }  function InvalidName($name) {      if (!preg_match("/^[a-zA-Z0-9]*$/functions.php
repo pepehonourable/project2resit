5589630f4b917622f2d6b998c0eb54f0f359bcb5
TerryIoannou <91316685+TerryIoannou@users.noreply.github.com>
2022-02-08 12:36:52 +0100

    Create functions.php

---
+++ code/<?php  function emptyInputRegister($name, $email, $pwd, $pwdRepeat) {      if (empty($name) || empty($email) || empty($pwd) || empty($pwdRepeat)) {         return true;     } else {         return false;     } }  function InvalidName($name) {      if (!preg_match("/^[a-zA-Z0-9]*$/functions.php
@@ -0,0 +1,116 @@
+<?php
+
+function emptyInputRegister($name, $email, $pwd, $pwdRepeat)
+{
+
+    if (empty($name) || empty($email) || empty($pwd) || empty($pwdRepeat)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function InvalidName($name)
+{
+
+    if (!preg_match("/^[a-zA-Z0-9]*$/", $name)) {
+
+        $result = true;
+    } else {
+        $result = false;
+    }
+    return $result;
+}
+function IsValidEmail($email)
+{
+
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+function invalidpwdMatch($pwd, $pwdRepeat)
+{
+
+    if ($pwd !== $pwdRepeat) {
+
+        $result = true;
+    } else {
+        $result = false;
+    }
+    return $result;
+}
+function UidExist($name, $conn, $email)
+{
+    $sql = "SELECT * FROM  users WHERE usersId = ? AND UsersEmail = ?";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        header("location: Register.php?error=stmtfailed");
+        exit();
+    }
+
+    mysqli_stmt_bind_param($stmt, "ss", $name, $email);
+    mysqli_stmt_execute($stmt);
+
+    $resultDATA = mysqli_stmt_get_result($stmt);
+
+    if ($row = mysqli_fetch_assoc($resultDATA)) {
+        return $row;
+    } else {
+        $result = false;
+        return $result;
+    }
+}
+function CreateUser($conn, $name, $email, $pwd)
+{
+    $sql = "INSERT INTO users(usersName, usersEmail, usersPwd) VALUES(?, ?, ?)";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        header("location: Register.php?error=stmtfailed");
+        exit();
+    }
+    $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+
+    mysqli_stmt_bind_param($stmt, "sss", $name, $email, $hashedPwd);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+    header("location: Register.php?error=none");
+}
+?>
+
+
+
+<?php
+function emptyInputLogin($name, $pwd)
+{
+
+    if (empty($name) || empty($pwd)) {
+        return false;
+    } else {
+        return true;
+    }
+    function loginUser($conn, $name, $pwd ,$email){
+
+        $idExist = UidExist($conn, $name, $email);
+        
+
+        if ($idExist === false) {
+            header("location: login.php?error=wronglogin");
+            
+        }
+        $pwdHashed = $idExist["usersPwd"];
+        $checkPwd = password_verify($pwd, $pwdHashed);
+
+        if ($checkPwd === false){
+            header("location: login.php?error=wronglogin");
+           
+        }
+        else if ($checkPwd === true){ 
+            session_start();
+            $_SESSION["userid"] = $idExist["usersId"];
+            header("location: index.php");
+        }
+    }
+}
+?>
