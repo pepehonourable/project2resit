a363854982045446d69e3094a1d5e87a11ae3ce4
Polina <91316935+Polinalavender@users.noreply.github.com>
2022-02-08 11:22:41 +0100

    Main Page

---
+++ code/MainPage.php
@@ -0,0 +1,45 @@
+<?php
+    include "header.php";
+    include "connect.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Orders</title>
+        <meta name="description" content="">
+        <link rel="stylesheet" href="stylesheet.css" type="text/css">
+    </head>
+    <body>
+        <div class="content">
+            <h1>Welcome to Vinyal Record Store!</h1>
+            <h3>Find any records you want!</h3>
+            <div class="records">
+                <div class = "record">
+                    <img src = "CODE/img.jpeg" alt="record">
+                </div>
+                <div class = "record">
+                    <img src = "CODE/img.jpeg" alt="record">
+                </div>
+                <div class = "record">
+                    <img src = "CODE/img.jpeg" alt="record">
+                </div>
+                <div class = "record">
+                    <img src = "CODE/img.jpeg" alt="record">
+                </div>
+                <div class = "record">
+                    <img src = "CODE/img.jpeg" alt="record">
+                </div>
+                <div class = "record">
+                    <img src = "CODE/img.jpeg" alt="record">
+                </div>
+                <div class = "record">
+                    <img src = "CODE/img.jpeg" alt="record">
+                </div>
+                <div class = "record">
+                    <img src = "CODE/img.jpeg" alt="record">
+                </div>
+            </div>
+        <?php include "footer.php" ?>
+    
