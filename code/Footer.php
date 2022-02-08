fb7390e95cbef384c0786ff5f5b68bc116037b38
Polina <91316935+Polinalavender@users.noreply.github.com>
2022-02-08 11:23:53 +0100

    Footer

---
+++ code/Footer.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Orders</title>
+        <meta name="description" content="">
+        <link rel="stylesheet" href="stylesheet.css" type="text/css">
+    </head>
+    <body>
+        <footer>
+          <div class= "footerright">
+             <p>&copy 2022 Bings Cars<p> 
+             <p>Send us a direct e-mail!</p>
+          </div>
+          <div class="footerleft">
+             <p>Monday 13:00-18:00</p>
+             <p>Tuesday 9:00-18:00</p>
+             <p>Wednessday 9:00-18:00</p>
+             <p>Thursday 9:00-21:00</p>
+             <p>Friday 9:00-21:00</p>
+             <p>Saturday 9:00-17:00</p>
+          </div>
+         </footer>
+      </div>
+    </body>
+</html>
+      
+     
