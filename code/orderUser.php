da575473f67db37ce0a917e7b3788bfe002e1584
pepehonourable <91469944+pepehonourable@users.noreply.github.com>
2022-02-08 19:51:37 +0100

    Add files via upload
    Now all products are grouped in one order and the results are sorted

--- code/orderUser.php
+++ code/orderUser.php
@@ -16,14 +16,14 @@
             <h1>Orders</h1>
             <div class="orders">
                 <?php
-                    $sql = "SELECT o.OrderId, p.Title, o.Delivered FROM orders o, product p WHERE o.ProductId=p.ProductId AND o.UserId=1";
+                    $sql = "SELECT l.OrderId, group_concat(' ',p.Title, ' (',l.Quantity,')') as Titles, o.Delivered FROM orders o, product p, orderLine l WHERE l.ProductId=p.ProductId AND l.OrderId=o.OrderId AND o.UserId=1 GROUP BY OrderId ORDER BY OrderId DESC";
                     $result = mysqli_query($conn, $sql);
                     if (mysqli_num_rows($result) > 0) {
                         while($row = mysqli_fetch_assoc($result))
                         if($row['Delivered'] == "No"){
-                            echo "<li>Order id: " . $row["OrderId"] . "<br> Album(s): " . $row["Title"] . "<br> Delivered: ". $row["Delivered"]. "<button></button> </li>";
+                            echo "<li>Order id: " .$row["OrderId"]. "<br> Album(s): " .$row["Titles"]. "<br> Delivered: " .$row["Delivered"]. "<button></button></li>";
                         } else {
-                          echo "<li>Order id: " . $row["OrderId"] . "<br> Album(s): " . $row["Title"] . "<br> Delivered: ". $row["Delivered"]. "</li>";
+                          echo "<li>Order id: " .$row["OrderId"]. "<br> Album(s): " .$row["Titles"]. "<br> Delivered: " .$row["Delivered"]. "</li>";
                         }
                       } else {
                         echo "0 results";
