9526aa4e671acd1bd0b6fff0291fdce16340d497
pepehonourable <91469944+pepehonourable@users.noreply.github.com>
2022-02-09 18:09:28 +0100

    Add files via upload
    Added first iteration of cart function to utils

--- code/utils.php
+++ code/utils.php
@@ -16,4 +16,49 @@
                         echo "0 results";
                       }
                     }
-                      ?>
+                    ?>
+
+<?php
+
+/*require "connect.php";
+function cart(){
+    global $conn;
+
+$id=1;
+
+$sql = "SELECT c.UserId, c.ProductId, c.Quantity, p.Title, p.Price FROM cart c, product p, user u WHERE c.UserId=u.UserId AND c.ProductId=p.ProductId AND $id=1";
+                    $result = mysqli_query($conn, $sql);
+                    if (mysqli_num_rows($result) > 0) {
+                        while($row = mysqli_fetch_assoc($result))
+                            echo "<li>" .$row["Title"]. "<br>Amount:".$row["Quantity"]. "<br>Price: " .$row["Price"]. "</li>";
+                        } else {
+                        echo "No items in cart!";
+                      }
+                    }
+                
+                    */
+
+require "connect.php";
+function cart(){
+global $conn;
+                    
+
+
+$sql = "SELECT c.UserId, c.ProductId, p.Title, p.Price, c.Quantity FROM cart c, product p, user u WHERE c.UserId=u.UserId AND c.ProductId=p.ProductId AND c.UserId=1";
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0) {
+    while($row = mysqli_fetch_assoc($result)){
+       
+        ?>
+        <tr>
+            <td> </td>
+            <td><?php echo $row["Title"]; ?></td>
+            <td><?php echo $row["Price"]; ?></td>
+            <td><input type="number" class="" value="<?php echo $row["Quantity"]; ?>" > </td>
+            <td><?php $cost=$row["Price"]*$row["Quantity"]; echo $cost?></td>
+        </tr>
+            <?php
+        } 
+        
+    }}
+?>
