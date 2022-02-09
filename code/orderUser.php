1ba609b384ab4735826b1dfef9a81cfd4200eb7a
pepehonourable <91469944+pepehonourable@users.noreply.github.com>
2022-02-09 14:02:16 +0100

    Add files via upload
    Clean up code and created a function to display orders, moved it to utils.php

--- code/orderUser.php
+++ code/orderUser.php
@@ -1,6 +1,7 @@
 <?php
     include "header.php";
     include "connect.php";
+    include "utils.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,18 +17,7 @@
             <h1>Orders</h1>
             <div class="orders">
                 <?php
-                    $sql = "SELECT l.OrderId, group_concat(' ',p.Title, ' (',l.Quantity,')') as Titles, o.Delivered FROM orders o, product p, orderLine l WHERE l.ProductId=p.ProductId AND l.OrderId=o.OrderId AND o.UserId=1 GROUP BY OrderId ORDER BY OrderId DESC";
-                    $result = mysqli_query($conn, $sql);
-                    if (mysqli_num_rows($result) > 0) {
-                        while($row = mysqli_fetch_assoc($result))
-                        if($row['Delivered'] == "No"){
-                            echo "<li>Order id: " .$row["OrderId"]. "<br> Album(s): " .$row["Titles"]. "<br> Delivered: " .$row["Delivered"]. "<button></button></li>";
-                        } else {
-                          echo "<li>Order id: " .$row["OrderId"]. "<br> Album(s): " .$row["Titles"]. "<br> Delivered: " .$row["Delivered"]. "</li>";
-                        }
-                      } else {
-                        echo "0 results";
-                      }
+                    order();
                 ?>
             </div>
         </div>
