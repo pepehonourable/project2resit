c5db6a5f00ee8479673374090b695fb75b73f15f
Polina <91316935+Polinalavender@users.noreply.github.com>
2022-02-13 19:28:54 +0100

    Update connect.php
    By Polly

--- code/connect.php
+++ code/connect.php
@@ -8,5 +8,9 @@ $conn = new mysqli($servername, $username, $password, "shop");
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
+$stmt = mysqli_prepare($conn, $query) OR DIE ("Preparation Error 1");
+            mysqli_stmt_execute($stmt) OR DIE ("Data retrieval error");
+            mysqli_stmt_bind_result($stmt, $id, $title, $artist, $videoid);
+            mysqli_stmt_store_result($stmt);
 
 ?> 
