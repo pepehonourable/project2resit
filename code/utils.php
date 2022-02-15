0f462e44325ba3dafcfa2b889fe72edd2493490d
pepehonourable <91469944+pepehonourable@users.noreply.github.com>
2022-02-15 23:37:36 +0100

    Add files via upload
    Added second function, will add delete.php and deleteOrder.php as functions here

--- code/utils.php
+++ code/utils.php
@@ -8,7 +8,8 @@
                     if (mysqli_num_rows($result) > 0) {
                         while($row = mysqli_fetch_assoc($result))
                         if($row['Delivered'] == "No"){
-                            echo "<li>Order id: " .$row["OrderId"]. "<br> Album(s):" .$row["Titles"]. "<br> Delivered: " .$row["Delivered"]. "     <button></button></li>";
+                            $_SESSION['Order'] = $row["OrderId"];
+                            echo "<li>Order id: " .$row["OrderId"]. "<br> Album(s):" .$row["Titles"]. "<br> Delivered: " .$row["Delivered"]. "<br><a href='deleteOrder.php'>Delete order</a></li>";
                         } else {
                           echo "<li>Order id: " .$row["OrderId"]. "<br> Album(s):" .$row["Titles"]. "<br> Delivered: " .$row["Delivered"]. "</li>";
                         }
@@ -20,45 +21,41 @@
 
 <?php
 
-/*require "connect.php";
-function cart(){
-    global $conn;
-
-$id=1;
-
-$sql = "SELECT c.UserId, c.ProductId, c.Quantity, p.Title, p.Price FROM cart c, product p, user u WHERE c.UserId=u.UserId AND c.ProductId=p.ProductId AND $id=1";
-                    $result = mysqli_query($conn, $sql);
-                    if (mysqli_num_rows($result) > 0) {
-                        while($row = mysqli_fetch_assoc($result))
-                            echo "<li>" .$row["Title"]. "<br>Amount:".$row["Quantity"]. "<br>Price: " .$row["Price"]. "</li>";
-                        } else {
-                        echo "No items in cart!";
-                      }
-                    }
-                
-                    */
-
 require "connect.php";
 function cart(){
 global $conn;
-                    
 
+$totalPrice = 0;
 
 $sql = "SELECT c.UserId, c.ProductId, p.Title, p.Price, c.Quantity FROM cart c, product p, user u WHERE c.UserId=u.UserId AND c.ProductId=p.ProductId AND c.UserId=1";
 $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
     while($row = mysqli_fetch_assoc($result)){
-       
         ?>
         <tr>
             <td> </td>
-            <td><?php echo $row["Title"]; ?></td>
+            <td><?php echo $row["Title"]; $_SESSION['Product'] = $row["ProductId"]; ?></td>
             <td><?php echo $row["Price"]; ?></td>
             <td><input type="number" class="" value="<?php echo $row["Quantity"]; ?>" > </td>
-            <td><?php $cost=$row["Price"]*$row["Quantity"]; echo $cost?></td>
+            <td><?php $cost = $row["Price"]*$row["Quantity"]; $_POST['$totalPrice'] = 3; echo "€" .$cost?></td>
+            <td><a href="delete.php">Delete product</a></td>
         </tr>
             <?php
-        } 
-        
-    }}
+            $totalPrice += ($cost);
+        }
+        echo "Total Price: €" . $totalPrice; 
+    }
+}
+?>
+<?php ;
+/*
+require "connect.php";
+function delete(){
+global $conn;
+
+$product = $_SESSION['Product'];
+mysqli_query($conn,"DELETE FROM cart WHERE UserId=1 AND ProductId=$product");
+$product = 0;
+}
+*/
 ?>
