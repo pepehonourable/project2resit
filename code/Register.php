29e10a3160f3f4e5aa717492df4d1d9f3479ee26
TerryIoannou <91316685+TerryIoannou@users.noreply.github.com>
2022-02-16 14:12:53 +0100

    Update Register.php

--- code/Register.php
+++ code/Register.php
@@ -4,18 +4,24 @@
 <head>
     <title>Register</title>
 </head>
-
+<link rel="stylesheet" href="../Styling/design.css">
 <body>
+<div class="positionBoxLogin">
     <section class="register-form">
+    <div class="LoginBox">
         <h1>Register Here</h1>
-        <div class="positionBoxLogin">
-            <div class="LoginBox">
                 <form action="register.php" method="post">
-                    <input type="text" name="name" placeholder="Full name...">
-                    <input type="email" name="email" placeholder="Email...">
-                    <input type="password" name="pwd" placeholder="Password...">
-                    <input type="password" name="pwdrepeat" placeholder="Repeat Password...">
-                    <button type="submit" name="submit">Sign Up</button>
+                    <input type="text" name="name" placeholder="Full name..." class="registerboxspacing">
+                    <br>
+                    <input type="number" name="age" placeholder="Age..." class="registerboxspacing">
+                    <br>
+                    <input type="email" name="email" placeholder="Email..." class="registerboxspacing">
+                    <br>
+                    <input type="password" name="pwd" placeholder="Password..." class="registerboxspacing">
+                    <br>
+                    <input type="password" name="pwdrepeat" placeholder="Repeat Password..." class="registerboxspacing">
+                    <br>
+                    <button type="submit" name="submit" class="submitforLogin">Sign Up</button>
                 </form>
             </div>
         </div>
@@ -27,26 +33,35 @@
 
         $name = $_POST["name"];
         $email = $_POST["email"];
+        $age = $_POST["age"];
         $pwd = $_POST["pwd"];
         $pwdRepeat = $_POST["pwdrepeat"];
 
         require_once 'db_conn.php';
         require_once 'functions.php';
 
-        if (emptyInputRegister($name, $email, $pwd, $pwdRepeat) == true) {
+        if (emptyInputRegister($name, $email,$age, $pwd, $pwdRepeat) == true) {
             header("location: Register.php?error=emptyinput");
             exit();
         }
 
         if (InvalidName($name) !== false) {
+            echo "Invalid ID";
             header("location: Register.php?error=invaliduid");
             exit();
         }
         if (IsValidEmail($email) == true) {
+            echo "Invalid Email";
             header("location: Register.php?error=invalidemail");
             exit();
         }
+        if(Invalidage($age) !==false){
+            echo "Invalid Age";
+            header("location: Register.php?error=Invalidage");
+            exit();
+        }
         if (invalidpwdMatch($pwd, $pwdRepeat) !== false) {
+            echo "Invalid Password";
             header("location: Register.php?error=invalidpwd");
             exit();
         }
