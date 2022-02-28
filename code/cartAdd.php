f0bfa137968be5088f4c231a90d8c7b4dba84283
pepehonourable <91469944+pepehonourable@users.noreply.github.com>
2022-02-28 15:27:27 +0100

    Add files via upload
    Adding items to cart
    Clear entire cart and process order in database

---
+++ code/cartAdd.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+require "connect.php";
+
+	//check if product is already in the cart
+	if(!in_array($_GET['id'], $_SESSION['cart'])){
+		array_push($_SESSION['cart'], $_GET['id']);
+		$_SESSION['message'] = 'Product added to cart';
+	}
+	else{
+		$_SESSION['message'] = 'Product already in cart';
+	}
+ 
+	header('location: main.php');
+
+?>
